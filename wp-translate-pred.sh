5d2d56529c66435464fd657aa64471f21db258b5
gibrown <greg@automattic.com>
2017-07-28 22:02:30 +0000

    Switch to a beam search width of 5

--- wp-translate-pred.sh
+++ wp-translate-pred.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 
-DEV_SOURCES=wp-data/wponly-processed/jetpack-es-source.txt
-
 MODEL_DIR=models/en2es-goldilocks
 PRED_DIR=${MODEL_DIR}/pred
 mkdir -p ${PRED_DIR}
+
+DEV_SOURCES=wp-data/wponly-processed/jetpack-2017-es-source.txt
+OUTFILE=${PRED_DIR}/jetpack-2017-pred.txt
 
 python bin/infer.py \
   --tasks "
     - class: DecodeText" \
   --model_dir $MODEL_DIR \
   --model_params "
-    inference.beam_search.beam_width: 3" \
+    inference.beam_search.beam_width: 5" \
   --input_pipeline "
     class: ParallelTextInputPipeline
     params:
       source_files:
         - $DEV_SOURCES" \
-  >  ${PRED_DIR}/predictions.txt
+  >  ${OUTFILE}

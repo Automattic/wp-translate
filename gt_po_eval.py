a710b02a06132a05e261279d68239474e07dca39
Greg Ichneumon Brown <gbrown5878@gmail.com>
2017-07-29 02:13:57 -0600

    hello world for google translate api working

---
+++ gt_po_eval.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+from __future__ import print_function
+import numpy as np
+from six.moves import range
+import wp_translate_util as util
+import sys
+
+################
+#
+# Take a .po translation file and encode it and convert it to parallel text format
+# because we are encoding all chars, we need to output individual numbers
+
+
+# Imports the Google Cloud client library
+from google.cloud import translate
+
+# Instantiates a client
+translate_client = translate.Client()
+
+# The text to translate
+text = u'Hello, world!'
+# The target language
+target = 'ru'
+
+# Translates some text into Russian
+translation = translate_client.translate(
+    text,
+    target_language=target)
+
+print(u'Text: {}'.format(text))
+print(u'Translation: {}'.format(translation['translatedText']))
+
+#if ( len(sys.argv) < 5 ):
+#    sys.exit( 'need to specify .po file, in charmap, out charmap, in file, out file' )
+#
+#po_file=sys.argv[1]
+#in_charmap_file=sys.argv[2]
+#out_charmap_file=sys.argv[3]
+#in_file=sys.argv[4]
+#out_file=sys.argv[5]
+#
+#MAXLENGTH = 500  #max length of input text
+#
+#print('Load Charmaps...')
+#intable = util.EncodedCharacterTable(in_charmap_file, MAXLENGTH)
+#outtable = util.EncodedCharacterTable(out_charmap_file, MAXLENGTH)
+#
+#print('Load Data...')
+#po_data = util.load_translated_po_data( po_file )
+#
+#ifh = open( in_file, 'w' )
+#ofh = open( out_file, 'w' )
+#
+#print('Generating...')
+#for i, t in enumerate(po_data):
+#    s_in = intable.encode_to_string(t.msgid)
+#    s_out = outtable.encode_to_string(t.msgstr)
+#    if ( ( s_in != None ) & ( s_out != None ) ):
+#    	ifh.write( s_in + "\n" )
+#    	ofh.write( s_out + "\n" )
+#
+#ifh.close()
+#ofh.close()
+#
+#print('Done.')

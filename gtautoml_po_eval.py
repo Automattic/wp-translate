385ffb1649650c728987a1446da1bca88e40edd8
Julian de Bhal <julian.debhal@automattic.com>
2018-09-25 16:02:06 +1000

    update gtautoml_po_eval.py (working)

--- gtautoml_po_eval.py
+++ gtautoml_po_eval.py
@@ -1,13 +1,16 @@
 # -*- coding: utf-8 -*-
 from __future__ import print_function
-import numpy as np
 from six.moves import range
 import polib as polib
 import sys
-from google.cloud import translate
+from google.cloud import automl_v1beta1
+
 import difflib
 from copy import deepcopy
 import time
+
+model_ids = { "es": "TRL113781196667304762" }
+project_id = 'machine-translation-api-216900'
 
 # we want to send batches to the translate api
 def batch_gen(data, batch_size):
@@ -22,15 +25,25 @@ def batch_gen(data, batch_size):
 if ( len(sys.argv) < 5 ):
     sys.exit( 'need to specify .po file, lang, out po file, diff file' )
 
+
 po_file=sys.argv[1]
 lang=sys.argv[2]
 out_file=sys.argv[3]
 diff_file=sys.argv[4]
 
+model_id = model_ids[lang] or sys.exit( 'no model for ' + lang)
+
 print('Load Data...')
 po = polib.pofile(po_file)
 
-translate_client = translate.Client()
+prediction_client = automl_v1beta1.PredictionServiceClient()
+name = 'projects/{}/locations/us-central1/models/{}'.format(project_id, model_id)
+params = {}
+def get_prediction(content):
+    payload = {'text_snippet': {'content': content}}
+    request = prediction_client.predict(name, payload, params)
+    return request.payload[0].translation.translated_content.content
+
 total = 0
 correct = 0
 untranslated = 0
@@ -46,51 +59,40 @@ entry = polib.POEntry(
     msgid=u'Welcome',
     msgstr=u'Bienvenue',
 )
-po.append(entry)
+# po.append(entry)
 
 print('Translating...')
 
-for entries in batch_gen(po, 20):
+for entry in po:
+    if(cnt > 10):
+        break
     if ( cnt % 100 == 0 ):
         print( total )
-    data = []
-    for entry in entries:
-    	data.append(entry.msgid)
+    t = None
+    while t == None:
+        try:
+            t = get_prediction(entry.msgid)
+        except:
+            print('Translation errror. pausing.', sys.exc_info()[0])
+            time.sleep(105)
 
-    try:
-    	translations = translate_client.translate(
-    	    data,
-    	    source_language='en',
-    	    target_language=lang,
-    	    model='nmt'
-    	)
-    except:
-        print('hit max usage. pausing')
-        time.sleep(105)
-        translations = translate_client.translate(
-    	    data,
-    	    source_language='en',
-    	    target_language=lang,
-    	    model='nmt'
-    	)
+    cnt = cnt + 1
+    out_entry = deepcopy(entry)
+    out_entry.msgstr=t
+    out_po.append(out_entry)
 
-    for entry,t in zip(entries, translations):
-        cnt = cnt + 1
-        out_entry = deepcopy(entry)
-	out_entry.msgstr=t['translatedText']
-	out_po.append(out_entry)
+    if ( entry.msgstr == '' ):
+        untranslated = untranslated + 1
+    else:
+        total = total + 1
+        if ( t == entry.msgstr ):
+            correct = correct + 1
+        else:
+            diff = difflib.ndiff([entry.msgstr], [t])
+            dfh.write( u"\n".join(diff) + u"\n\n" )
 
-	if ( entry.msgstr == '' ):
-	    untranslated = untranslated + 1
-	else:
-	    total = total + 1
-	    if ( t['translatedText'] == entry.msgstr ):
-	        correct = correct + 1
-	    else:
-	        diff = difflib.ndiff([entry.msgstr], [t['translatedText']])
-	        dfh.write( u"\n".join(diff).encode('utf-8') + "\n\n" )
-
-po.save(out_file)
+print('Done! Saving to ' + out_file)
+out_po.save(out_file)
 
 dfh.close()
 
